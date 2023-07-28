bb0b19f38fc24c3fed0fa6438d1e231a92185d4d
Catherine Wrenhold <40322939+cwrenhold@users.noreply.github.com>
2023-07-28 21:25:20 +0100

    Added script loading from file, and using a script parameter to specify the file to load.

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -11,9 +12,23 @@ import (
 	"github.com/jmoiron/sqlx"
 )
 
+type SqlScript int
+
+const (
+	select_tasks SqlScript = iota
+	select_tasks_active
+)
+
+var sqlScripts = map[string]SqlScript{
+	"select_tasks":        select_tasks,
+	"select_tasks_active": select_tasks_active,
+}
+
 func main() {
 	http.HandleFunc("/", indexHandler)
 	http.HandleFunc("/test", testHandler)
+	http.HandleFunc("/test-file", testFileHandler)
+	http.HandleFunc("/test-file-script", testFileScriptHandler)
 
 	port := os.Getenv("WEB_PORT")
 	log.Printf("Listening on port %s", port)
@@ -48,11 +63,69 @@ func testHandler(w http.ResponseWriter, r *http.Request) {
 
 	defer rows.Close()
 
+	jsonData := readRowsToJsonArray(rows)
+
+	w.Write(jsonData)
+}
+
+func testFileHandler(w http.ResponseWriter, r *http.Request) {
+	initializers.ConnectToDB()
+
+	data, err := os.ReadFile("scripts/select_tasks.sql")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	query := string(data)
+
+	jsonData := runQueryToJson(query)
+
+	w.Write(jsonData)
+}
+
+func testFileScriptHandler(w http.ResponseWriter, r *http.Request) {
+	initializers.ConnectToDB()
+
+	queryParams := r.URL.Query()
+
+	rawScriptType := queryParams["script"][0]
+	_, ok := sqlScripts[rawScriptType]
+
+	if !ok {
+		log.Fatal("Invalid script type")
+	}
+
+	data, err := os.ReadFile(fmt.Sprintf("scripts/%s.sql", rawScriptType))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	query := string(data)
+
+	jsonData := runQueryToJson(query)
+
+	w.Write(jsonData)
+}
+
+func runQueryToJson(query string) []byte {
+	rows, err := initializers.SqlxDB.Queryx(query)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer rows.Close()
+
+	return readRowsToJsonArray(rows)
+}
+
+func readRowsToJsonArray(rows *sqlx.Rows) []byte {
 	var results []string
 
 	for rows.Next() {
 		result := make(map[string]interface{})
-		err = rows.MapScan(result)
+		err := rows.MapScan(result)
 		if err != nil {
 			log.Fatalln(err)
 		}
@@ -65,7 +138,5 @@ func testHandler(w http.ResponseWriter, r *http.Request) {
 		results = append(results, string(jsonData))
 	}
 
-	jsonData := []byte("[" + strings.Join(results, ",") + "]")
-
-	w.Write(jsonData)
+	return []byte("[" + strings.Join(results, ",") + "]")
 }
---
+++ scripts/select_tasks_active.sql
@@ -0,0 +1,1 @@
+SELECT * FROM tasks WHERE is_complete = false;
